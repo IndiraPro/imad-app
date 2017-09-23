84a80764b2d6ec35811ac08b238bac14766c5d65
IndiraPro <IndiraPro@users.noreply.github.com>
2017-09-23 08:07:31 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two', function (req, res) {
   res.send('Article two');
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article three');
 });
 
